51b29f96bde60bd920c67fb2a548043b6ff1cb00
mandahumada <amandahumada07@gmail.com>
2024-10-01 09:54:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def index():
 
 # Ruta para guardar la inscripción a un curso
 @app.route("/inscripciones/guardar", methods=["POST"])
-def inscripciones_guardar():
+def inscripcion_guardar():
     nombre_curso = request.form["slNombreCurso"]
     telefono = request.form["txtTelefono"]
 
